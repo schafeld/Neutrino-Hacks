256bb1dd2621394cf34c880df08387bb50b83a1a
Oliver Schafeld <oliver@schafeld.com>
2017-03-07 19:13:28 +0100

    initial setup, first working version
    first working version from Source:
    https://hacks.mozilla.org/2017/02/using-neutrino-for-modern-javascript-d
    evelopment/

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* Source:  https://hacks.mozilla.org/2017/02/using-neutrino-for-modern-javascript-development/ */
+
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<h1>Go for React!</h1>, document.getElementById('root'));
