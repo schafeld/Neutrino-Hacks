463b7f9df0db0a5aec0d016517ac95a6e7bc67b8
Oliver Schafeld <oliver@schafeld.com>
2017-03-12 20:41:55 +0100

    add Stateless Function Component alternative to accordionComponent
    A Class Component can have state, while a Stateless Function Component
    cannot. Also one has to use „className“ instead of „class“ for CSS.

--- src/accordionComponent.js
+++ src/accordionComponent.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+
+// Class Component (can have state)
 class AccordionComponent extends React.Component {
   render() {
-    return <div>some string</div>;
+    return <div className="text-box">some string</div>;
   }
 }
+
+
+// Alternatively a Stateless Function Component would look like this:
+/*
+const AccordionComponent = () => <div className="text-box">some string here too</div>;
+*/
 
 export default AccordionComponent;
