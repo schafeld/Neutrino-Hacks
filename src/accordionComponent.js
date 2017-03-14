4521a5faa2cf96b8f68c1b835d25178c82dca310
Oliver Schafeld <oliver@schafeld.com>
2017-03-14 20:37:45 +0100

    add state var with input field attached

--- src/accordionComponent.js
+++ src/accordionComponent.js
@@ -3,14 +3,28 @@ import React from 'react';
 
 // Class Component (can have state)
 class AccordionComponent extends React.Component {
+  // state
+  constructor() {
+    super();
+    this.state = {
+      txt: 'This is the state text',
+    };
+  }
+
+  update(e) {
+    this.setState({ txt: e.target.value });
+  }
+
   render() {
     const author = this.props.author;
     // There must be exactly 1 outer wrapping tag.
     // Parentheses are rquired if JSX isn't  in same line as return statement.
     return (
         <div className="wrapper">
-            <div classsName="text-box">
-                some string
+          <input type="text"
+            onChange={this.update.bind(this)} />
+            <div className="another-text-box">
+                {this.state.txt} &amp; some string
             </div>
             <div className="text-box">
                 some other string, authored by {author}
